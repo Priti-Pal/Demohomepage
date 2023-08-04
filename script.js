028a928022497fb51a0ecc27450af124598fb6d1
Priti-Pal <121653067+Priti-Pal@users.noreply.github.com>
2023-08-04 18:58:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const myButtonA = document.getElementById('myButtonA');
+myButtonA.addEventListener('click', () => {
+    
+    alert('Button clicked!');
+    
+});
+const myButtonI = document.getElementById('myButtonI');
+myButtonI.addEventListener('click', () => {
+    
+    alert('Button clicked!');
+    
+});
+const myButtonU = document.getElementById('myButtonU');
+myButtonU.addEventListener('click', () => {
+    
+    alert('Button clicked!')
+});
+    
+// });
